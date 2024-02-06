d685fa707366c4d3b98f9a5a23a54c1e2b3a1199
hartoley <tolanijimoh1@gmail.com>
2024-02-06 10:15:38 +0100

    Keena calc

---
+++ monday.js
@@ -0,0 +1,18 @@
+
+
+var drink = "fanta"
+var meal ="fried rice"
+
+console.log(drink)
+console.log(meal)
+
+alert(drink)
+
+const breakfast = "everybody"
+
+console.log(breakfast)
+
+console.log(typeof drink)
+
+drink = "coke"
+console.log(drink)
---
+++ tue.js
@@ -0,0 +1,5 @@
+let namess= 'keena'
+
+console.log(typeof namess)
+let a='5'
+
